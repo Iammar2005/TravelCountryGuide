1838a1304595763bab39a8f5f204680bfc379157
Iammar2005 <ammar.ime.05@gmail.com>
2026-06-01 14:53:56 +0200

    Create initial project structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+      <header className="header">
+        <h1>Travel Country Guide</h1>
+  
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Favorites</a>
+          <a href="#">Wishlist</a>
+          <a href="#">Compare</a>
+          <a href="#">Quiz</a>
+        </nav>
+      </header>
+    )
+  }
+  
+  export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+      <section className="home">
+        <h2>Explore countries around the world</h2>
+        <p>
+          Search for countries, view important information, save favorites and plan countries you want to visit.
+        </p>
+      </section>
+    )
+  }
+  
+  export default Home
